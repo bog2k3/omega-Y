0bc20dee57cfa4d4770260e382f46285ac1a6d0d
Bogdan I <bog2k3@gmail.com>
2019-01-15 23:11:14 +0200

    +linux rp3d lib; +MSAA

--- src/main.cpp
+++ src/main.cpp
@@ -208,7 +208,8 @@ int main(int argc, char* argv[]) {
 
 		// initialize stuff:
 		int winW = 1024, winH = 768;
-		if (!gltInitGLFW(winW, winH, "Omega-Y"))
+		int multisamples = 4; // 0 to disable MSAA or >0 to enable it
+		if (!gltInitGLFW(winW, winH, "Omega-Y", multisamples))
 			return -1;
 
 		GLFWInput::initialize(gltGetWindow());
