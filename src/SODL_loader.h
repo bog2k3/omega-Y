216a87dc678dc027aee5128180e117cadc87ff4a
Bogdan Ionita <bog2k3@gmail.com>
2019-07-01 17:03:09 +0300

    SODL prototyping...

---
+++ src/SODL_loader.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/*
+	SODL_loader.h
+
+	@author: bogdan ionita <bog2k3@gmail.com>
+	@date: July 7, 2019
+
+	SODL - Simplified Object Description Language
+	This class implements an object loader from a SODL file
+*/
+
+class SODL_Loader {
+public:
+
+private:
+};
